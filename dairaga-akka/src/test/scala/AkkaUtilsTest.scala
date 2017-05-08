cc6d44909a314801609caa77666af26bfce13489
kigi <kigi.chang@gmail.com>
2017-05-08 22:35:24 +0800

    add AkkaUtils Unit Test

---
+++ dairaga-akka/src/test/scala/AkkaUtilsTest.scala
@@ -0,0 +1,36 @@
+import dairaga.akka.AkkaUtils
+import dairaga.env._
+import dairaga.key._
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.immutable
+import scala.util.Try
+
+/**
+  * Created by kigi on 5/8/17.
+  */
+class AkkaUtilsTest extends FlatSpec with Matchers {
+
+  "AkkaUtils.cluster" should "initialize a one node cluster" in {
+    val tmp = Try {
+      AkkaUtils.cluster(immutable.Seq.empty[String])
+    }
+
+    tmp.isSuccess should === (true)
+
+    tmp.get.settings.systemName should === (AkkaClusterName)
+    AkkaUtils.shutdown(tmp.get)
+  }
+
+  "AkkaUtils.cluster" should "read ip from configuration" in {
+    val tmp = Try {
+      AkkaUtils.cluster(immutable.Seq.empty[String], "test-ip")
+    }
+
+    tmp.isSuccess should === (true)
+    val cluster = tmp.get
+    cluster.selfAddress.host.get shouldNot be ("127.0.0.1")
+    AkkaUtils.shutdown(tmp.get)
+  }
+}
