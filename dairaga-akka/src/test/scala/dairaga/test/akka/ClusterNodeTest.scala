53d0743c3aa046f153e1fb81f596e8a5da55f3dd
kigi <kigi.chang@gmail.com>
2017-06-02 23:45:24 +0800

    add hook function tests

--- dairaga-akka/src/test/scala/dairaga/test/akka/ClusterNodeTest.scala
+++ dairaga-akka/src/test/scala/dairaga/test/akka/ClusterNodeTest.scala
@@ -1,6 +1,6 @@
 package dairaga.test.akka
 
-import akka.actor.{Actor, ActorContext, ActorRef, Address, Inbox, Props}
+import akka.actor.{Actor, ActorContext, ActorRef, Address, Inbox, PoisonPill, Props}
 import akka.cluster.pubsub.DistributedPubSubMediator.Publish
 import akka.testkit.{ImplicitSender, TestKit}
 import dairaga.akka.{ClusterNode, DairagaActor, XVShutdown}
@@ -24,12 +24,42 @@ object TestNode extends ClusterNode {
       override def receive: Receive = {
         case msg: String => sender() ! msg
 
-        case XVShutdown =>
-          mediator ! Publish(dairaga.env.XVClusterInfo, XVShutdown)
+        //case XVShutdown =>
+        //  mediator ! Publish(dairaga.env.XVClusterInfo, XVShutdown)
       }
     }))
 
     context.watch(actor)
+  }
+
+  var callInternPostStop: Boolean = false
+  override def internPostStop(context: ActorContext): Unit = {
+    callInternPostStop = true
+    super.internPostStop(context)
+  }
+
+  var callChildOnTerminated: Boolean = false
+  override def childOnTerminated(context: ActorContext, actor: ActorRef): Unit = {
+    super.childOnTerminated(context, actor)
+    callChildOnTerminated = true
+  }
+
+  var callPreStart: Boolean = false
+  override def preStart(): Unit = {
+    super.preStart()
+    callPreStart = true
+  }
+
+  var callAfterRun: Boolean = false
+  override def afterRun(): Unit = {
+    super.afterRun()
+    callAfterRun = true
+  }
+
+  var callPostStop: Boolean = false
+  override def postStop(): Unit = {
+    callPostStop = true
+    super.postStop()
   }
 
   run()
@@ -43,25 +73,36 @@ class ClusterNodeTest extends TestKit(TestNode.system) with ImplicitSender with 
 
   "a simple node" must {
     "have correct status" in {
+
+      awaitCond(TestNode.actor != null)
+
       TestNode.initialized should === (false)
       TestNode.started should === (true)
       TestNode.terminated === (false)
+
+      TestNode.callPreStart should === (true)
+      TestNode.callAfterRun should === (true)
     }
 
     "echo msg" in {
       val msg = "hello"
-
-      awaitCond(TestNode.actor != null)
 
       TestNode.actor ! msg
 
       expectMsg(msg)
     }
 
+    "terminate a child actor" in {
+      TestNode.actor ! PoisonPill
+      awaitCond(TestNode.callChildOnTerminated == true)
+    }
+
     "be terminated when got XVShutdown" in {
-     TestNode.actor ! XVShutdown
+     TestNode.intern ! XVShutdown
 
       awaitCond(TestNode.terminated == true)
+      TestNode.callInternPostStop should === (true)
+      TestNode.callPostStop should === (true)
     }
   }
 
