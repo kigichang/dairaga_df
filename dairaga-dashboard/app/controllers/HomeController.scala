faf276f89ddd47234c1e0bf90df379b5d3fa82eb
kigi <kigi.chang@gmail.com>
2017-07-08 22:34:23 +0800

    dashboard project upgrade to 2.6.1

--- dairaga-dashboard/app/controllers/HomeController.scala
+++ dairaga-dashboard/app/controllers/HomeController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 /**
@@ -12,13 +11,13 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
+   * Create an Action to render an HTML page with a welcome message.
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
   }
+
 }
